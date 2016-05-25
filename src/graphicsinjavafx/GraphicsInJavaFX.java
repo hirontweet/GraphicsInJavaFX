7aaa6941fccfabdb450afb264ac5b98c7bd9e967
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-25 15:06:48 +0900

    Shapeクラスを使って簡単なサンプルの描画を行った
    Shapeクラスを用いて描画を行った。Shapeクラスは javafx.scene.shapeに定義されている
    クラスをインスタンス化して属性を設定して、Groupクラスに閉じ込めて描画を行う。
    Shapeクラスはたくさんのものを描画するときは得意ではない模様。

--- src/graphicsinjavafx/GraphicsInJavaFX.java
+++ src/graphicsinjavafx/GraphicsInJavaFX.java
@@ -6,6 +6,12 @@
 package graphicsinjavafx;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 /**
@@ -17,6 +23,23 @@ public class GraphicsInJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         
+        Circle circle = new Circle(150, 150, 50);
+        circle.setFill(Color.BLUE);
+        
+        Ellipse ellipse = new Ellipse(150, 45, 60, 30);
+        ellipse.setFill(Color.CRIMSON);
+        
+        Line line1 = new Line(10, 80, 200, 80);
+        line1.setStroke(Color.BLACK);
+        Line line2 = new Line(10, 90, 200, 90);
+        line2.setStroke(Color.BLUEVIOLET);
+        
+        Group root = new Group();
+        root.getChildren().addAll(circle, ellipse, line1, line2);
+        
+        Scene scene = new Scene(root, 300, 200);
+        
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Hello World!");
         primaryStage.show();
     }
