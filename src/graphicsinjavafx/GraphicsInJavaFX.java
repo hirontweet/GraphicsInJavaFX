a24ee6cdc7fe7d25b47db63295dac0449d7f0c6c
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-25 15:56:38 +0900

    Canvasクラスを使って描画した
    Canvasクラスを用いて描画を行った。
    Canvasを使う場合、サイズを指定してインスタンスを得た後に、GraphicsContextを取得、
    取得したGraphicsContextに対して、用意されている関数で描画を行う。

--- src/graphicsinjavafx/GraphicsInJavaFX.java
+++ src/graphicsinjavafx/GraphicsInJavaFX.java
@@ -6,6 +6,11 @@
 package graphicsinjavafx;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -17,6 +22,27 @@ public class GraphicsInJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         
+        final Canvas canvas = new Canvas(300, 200);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        Group root = new Group();
+        
+        gc.setLineWidth(2.0);
+        // Strokeの色の設定(BLACK)
+        gc.setStroke(Color.BLACK);
+        gc.strokeLine(10, 10, 230, 10);
+        
+        // Fillの色を設定(DARKORANGE)
+        gc.setFill(Color.DARKORANGE);
+        gc.fillRect(15, 25, 100, 100);
+        
+        // Strokeの色を変更(BLACK -> GREEN)
+        gc.setStroke(Color.GREEN);
+        gc.strokeOval(150, 40, 50, 80);
+        
+        root.getChildren().add(canvas);
+        
+        primaryStage.setScene(new Scene(root, 300, 200));
         primaryStage.setTitle("Hello World!");
         primaryStage.show();
     }
