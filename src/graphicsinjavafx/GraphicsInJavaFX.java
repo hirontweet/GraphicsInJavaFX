2787dbe61fb7090d46be56e88de7f2f3e0c41fc8
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-25 14:55:21 +0900

    プロジェクトをはじめた
    Netbeansのデフォルトテンプレートより少し変更してコミット

---
+++ src/graphicsinjavafx/GraphicsInJavaFX.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphicsinjavafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author spaism
+ */
+public class GraphicsInJavaFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
