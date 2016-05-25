16675c0df7c9882b3363718d3a63358e4e29e26d
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-25 18:39:16 +0900

    ドラッグ操作を実装した
    下記のリンクを参考に、ドラッグ操作を実装した。
    現在の実装では、Shapeクラスを使って書いているのがわかる。
    
    http://java-buddy.blogspot.jp/2013/07/javafx-drag-and-move-something.html

--- src/graphicsinjavafx/GraphicsInJavaFX.java
+++ src/graphicsinjavafx/GraphicsInJavaFX.java
@@ -6,6 +6,12 @@
 package graphicsinjavafx;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -14,9 +20,28 @@ import javafx.stage.Stage;
  */
 public class GraphicsInJavaFX extends Application {
     
+    Circle circle_red, circle_blue;
+    double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
+    
     @Override
     public void start(Stage primaryStage) {
         
+        circle_red = new Circle(100, 100, 50);
+        circle_red.setFill(Color.RED);
+        circle_red.setOnMousePressed(circlePressedHandler);
+        circle_red.setOnMouseDragged(circleDraggedHandler);
+        
+        circle_blue = new Circle(250, 250, 50);
+        circle_blue.setFill(Color.BLUE);
+        circle_blue.setOnMousePressed(circlePressedHandler);
+        circle_blue.setOnMouseDragged(circleDraggedHandler);
+        
+        Group root = new Group();
+        root.getChildren().addAll(circle_red, circle_blue);
+        
+        Scene scene = new Scene(root, 500, 400);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Hello World!");
         primaryStage.show();
     }
@@ -27,5 +52,33 @@ public class GraphicsInJavaFX extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    
+    EventHandler<MouseEvent> circlePressedHandler = new EventHandler<MouseEvent>(){
+    
+        @Override
+        public void handle(MouseEvent e){
+            orgSceneX = e.getSceneX();
+            orgSceneY = e.getSceneY();
+            orgTranslateX = ((Circle)(e.getSource())).getTranslateX();
+            orgTranslateY = ((Circle)(e.getSource())).getTranslateY();
+        }
+    };
+    
+    
+    EventHandler<MouseEvent> circleDraggedHandler = new EventHandler<MouseEvent>(){
+        
+        @Override
+        public void handle(MouseEvent e){
+            double offsetX = e.getSceneX() - orgSceneX;
+            double offsetY = e.getSceneY() - orgSceneY;
+            double newTranslateX = orgTranslateX + offsetX;
+            double newTranslateY = orgTranslateY + offsetY;
+            
+            ((Circle)(e.getSource())).setTranslateX(newTranslateX);
+            ((Circle)(e.getSource())).setTranslateY(newTranslateY);
+        }
+    };
+    
     
 }
